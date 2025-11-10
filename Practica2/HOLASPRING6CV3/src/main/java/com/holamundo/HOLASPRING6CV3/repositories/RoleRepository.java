735c887b36bd6c75430cde3773dd5ab49d2e424f
antony25810 <antonyhorteales@gmail.com>
2025-11-10 17:48:51 -0600

    Merge de remoto main con histórico local

--- Practica2/HOLASPRING6CV3/src/main/java/com/holamundo/HOLASPRING6CV3/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.holamundo.HOLASPRING6CV3.repositories;
-
-import com.holamundo.HOLASPRING6CV3.models.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Optional<Role> findByName(String name);
-}
